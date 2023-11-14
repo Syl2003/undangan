251a8013ab6ba97aebc0bf2c2b6422625ce97d48
syl2003 <khususgithub2003@gmail.com>
2023-11-14 17:10:26 +0700

    update undangan pernikahan

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector('a');
 const home = document.querySelector('.home');
+const hal1 = document.querySelector('.hal1');
 
 btn.addEventListener('click', () => {
     home.style.translate = '0 -100%';
+    hal1.style.display = 'block'
 });
