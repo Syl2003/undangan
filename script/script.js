b775132b3d1f6ab49a05aaa2786af807f5c35037
Syul2003 <syul2003@gmail.com>
2023-11-16 01:38:44 +0700

    update undangan pernikahan

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
 const btn = document.querySelector('a');
 const home = document.querySelector('.home');
+const contai = document.querySelector('.contai');
 const hal1 = document.querySelector('.hal1');
 
 btn.addEventListener('click', () => {
     home.style.translate = '0 -100%';
+    contai.style.display = "block";
     hal1.style.display = 'grid';
 });
