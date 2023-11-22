0e5dd0129ebf720b6aeb39546af075b645b6a550
Syl2003 <syl2003.com>
2023-11-22 18:22:31 +0700

    update undangan pernikahan

--- script/script.js
+++ script/script.js
@@ -2,13 +2,27 @@ const btn = document.querySelector('a');
 const home = document.querySelector('.home');
 const contai = document.querySelector('.contai');
 const hal1 = document.querySelector('.hal1');
+const music = document.querySelector('.music');
+const myMusic = document.querySelector('.my-music');
 const btnCopy1 = document.getElementById('btnCopy1');
 const btnCopy2 = document.getElementById('btnCopy2');
+
+const audio = new Audio('https://tikcdn.io/ssstik/aHR0cHM6Ly9zZjE2LWllcy1tdXNpYy50aWt0b2tjZG4uY29tL29iai9pZXMtbXVzaWMtYWlzby83MDA0ODExMjIwODM4OTc2MjgyLm1wMw==');
 
 btn.addEventListener('click', () => {
     home.style.translate = '0 -100%';
     contai.style.display = "block";
     hal1.style.display = 'grid';
+    music.style.display = 'block';
+      audio.play();
+    
+    myMusic.addEventListener('click', () => {
+      if(audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    });
 });
 
 function copy1() {
