06467ed3b38199c391754c32e2b8f1f3d1e45633
syl2003 <khususgithub2003@gmail.com>
2023-11-22 19:25:04 +0700

    update undangan pernikahan

--- script/script.js
+++ script/script.js
@@ -6,39 +6,45 @@ const music = document.querySelector('.music');
 const myMusic = document.querySelector('.my-music');
 const btnCopy1 = document.getElementById('btnCopy1');
 const btnCopy2 = document.getElementById('btnCopy2');
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
 
 const audio = new Audio('https://tikcdn.io/ssstik/aHR0cHM6Ly9zZjE2LWllcy1tdXNpYy50aWt0b2tjZG4uY29tL29iai9pZXMtbXVzaWMtYWlzby83MDA0ODExMjIwODM4OTc2MjgyLm1wMw==');
 
 btn.addEventListener('click', () => {
-    home.style.translate = '0 -100%';
-    contai.style.display = "block";
-    hal1.style.display = 'grid';
-    music.style.display = 'block';
+  home.style.translate = '0 -100%';
+  contai.style.display = "block";
+  hal1.style.display = 'grid';
+  play.remove()
+  music.style.display = 'block';
+  audio.play();
+
+  myMusic.addEventListener('click', () => {
+    if (audio.paused) {
       audio.play();
-    
-    myMusic.addEventListener('click', () => {
-      if(audio.paused) {
-        audio.play();
-      } else {
-        audio.pause();
-      }
-    });
+      play.add()
+    } else {
+      audio.pause();
+      play.add()
+      pause.add()
+    }
+  });
 });
 
 function copy1() {
-    navigator.clipboard.writeText(document.getElementById('txt1').innerHTML);
-    
-    btnCopy1.innerHTML = "copy berhasil"
-    setTimeout( () => {
-      btnCopy1.innerHTML = "copy rekening"
-    },900);
+  navigator.clipboard.writeText(document.getElementById('txt1').innerHTML);
+
+  btnCopy1.innerHTML = "copy berhasil"
+  setTimeout(() => {
+    btnCopy1.innerHTML = "copy rekening"
+  }, 900);
 }
 
 function copy2() {
-    navigator.clipboard.writeText(document.getElementById('txt2').innerHTML);
-    
-    btnCopy2.innerHTML = "copy berhasil"
-    setTimeout( () => {
-      btnCopy2.innerHTML = "copy rekening"
-    },900);
+  navigator.clipboard.writeText(document.getElementById('txt2').innerHTML);
+
+  btnCopy2.innerHTML = "copy berhasil"
+  setTimeout(() => {
+    btnCopy2.innerHTML = "copy rekening"
+  }, 900);
 }
